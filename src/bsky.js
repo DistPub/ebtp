b037eaddcf4a78a9260b410eac4d166e76944ca6
Smite Chow <xiaopengyou@live.com>
2025-04-02 12:11:54 +0000

    fix 500 on worker

--- src/bsky.js
+++ src/bsky.js
@@ -8,9 +8,14 @@ export async function handleRequest(request, env, ctx) {
   url.protocol = 'https:'
   url.port = ''
 
+  // note: direct use request got random 500, so copy headers
+  const labelers = request.headers.get('atproto-accept-labelers')
+  let headers = {}
+  if (labelers) headers = {'atproto-accept-labelers': labelers}
+
   if (url.host === 'api.hukoubook.com') {
-		return env.blueskyapi.fetch(new Request(url.toString(), request))
+		return env.blueskyapi.fetch(new Request(url.toString(), {headers}))
 	} else {
-    return fetch(new Request(url.toString(), request))
+    return fetch(new Request(url.toString(), {headers}))
   }
 }
